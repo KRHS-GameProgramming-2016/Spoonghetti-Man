4be1c579884ba7b52206775ca15005f2dfe69802
RussellMellott <russell.mellott@kearsarge.org>
2017-01-19 11:12:21 -0500

    level design

--- Wall.py
+++ Wall.py
@@ -1,7 +1,7 @@
 
 import pygame, sys, math
 class Wall():
-    def __init__(self, pos, tileSize=50):
+    def __init__(self, pos, tileSize=40):
         self.image = pygame.image.load("rsc/wall/wall.png")
         self.image = pygame.transform.scale(self.image, [tileSize,tileSize])
         self.rect = self.image.get_rect(center = pos)
