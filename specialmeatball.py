9d8f1be933051ae0fd23e95b07edb9df39d2629d
RussellMellott <russell.mellott@kearsarge.org>
2017-02-10 11:13:12 -0500

    ball

--- specialmeatball.py
+++ specialmeatball.py
@@ -3,6 +3,8 @@ from Meatball import *
 
 class Specialmeatball(Meatball):
     def __init__(self, pos=[0,0], size=None):
-        self.image = pygame.image.load("rsc/ball/spicy.png")
         Meatball.__init__(self, pos, size)
+        self.image = pygame.image.load("rsc/ball/spicy.png")
+        if size:
+            self.image = pygame.transform.scale(self.image, [size,size])
         self.points = 2
