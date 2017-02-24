78be2bc84605fe257ab7b4c6c322b726a9044c6e
Ethan <EthanJ.Thompson@kearsarge.org>
2017-02-24 11:02:03 -0500

    Bleeb Blob

--- Wall.py
+++ Wall.py
@@ -2,6 +2,6 @@
 import pygame, sys, math
 class Wall():
     def __init__(self, pos, tileSize=40):
-        self.image = pygame.image.load("rsc/wall/garlic.png")
+        self.image = pygame.image.load("rsc/wall/SpoonerF(1).png")
         self.image = pygame.transform.scale(self.image, [tileSize,tileSize])
         self.rect = self.image.get_rect(center = pos)
