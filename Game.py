814155efbba637579d71bf757b0471f52a25b224
Ethan <EthanJ.Thompson@kearsarge.org>
2017-03-16 11:17:55 -0400

    Player 2 wins now

--- Game.py
+++ Game.py
@@ -120,10 +120,10 @@ while True:
     lev += 1
     scoreScreen = True
     if score > score2:
-        bgImage = pygame.image.load ("Levels/Player 1 wins.png")
+        bgImage = pygame.image.load ("Levels/Player 2 wins2.png")
         bgRect = bgImage.get_rect()
     else:
-        bgImage = pygame.image.load ("Levels/Player 2 wins.png")
+        bgImage = pygame.image.load ("Levels/Player 1 wins.png")
         bgRect = bgImage.get_rect()
 
     while scoreScreen:
